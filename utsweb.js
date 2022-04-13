11663661168656f78fba7f62acb0c22a080538c1
Rayhan <93690158+Rayhan666-dot@users.noreply.github.com>
2022-04-13 10:50:06 +0800

    js

---
+++ utsweb.js
@@ -0,0 +1,15 @@
+let box1 = document.getElementById('box1');
+
+document.onmousemove = (event) => {
+    var x = event.clientX;
+    var y = event.clientY;
+    box1.style.left = x - (box1.clientWidth/2) + "px";
+    box1.style.top = y - (box1.clientWidth/2) + "px";
+    }
+function random(number){
+    return Math.floor(Math.random() * number)
+}
+document.onclick = function () {
+ubah = "rgb("+random(255)+", "+random(255)+", "+random(255)+")";
+box1.style.backgroundColor = ubah;
+}
